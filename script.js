f9d5549c2e317dc24163e1044623b4cdd25fb002
Rika Kamila <rikakamila1717@gmail.com>
2024-09-09 21:39:13 +0700

    33 menit

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
 let number1 = [1,4,6,7]
 
-
 function numberKaliDua (arr) {
     return arr.map(num => num *2);
 }
 
-let hasil1 = numberkalidua(number1);
-let hasil2 = numberkalidua(number2);
-let hasil3 = numberkalidua(number3);
-let hasil4 = numberkalidua(number4);
+let hasil1 = numberKaliDua(number1);
+
 
 console.log(hasil1);
 console.log(hasil2);
@@ -16,20 +13,16 @@ console.log(hasil3);
 console.log(hasil4);
 
 
-
-let number1 = [1,4,6,7]
-let number2 = [7,4,2,1]
-let number3 = [5]
-let number4 = [1,3]
+let arrSalinan = [...numberHasil]
 
 function numberUrut (arr) {
+    let arrSalinan = [arr];
     return arrSalinan.reverse ();  
-
 }
 
 
-   let hasil1 = numberkalidua(number1);
-let hasil2 = numberkalidua(number2);
-let hasil3 = numberkalidua(number3);
-let hasil4 = numberkalidua(number4);
+let hasil1 = numberUrut(number1);
+let hasil2 = numberUrut(number2);
+let hasil3 = numberUrut(number3);
+let hasil4 = numberUrut(number4);
 
