6d70a081d3dca0b6da8c7a49c541f173c631dde4
Rika Kamila <rikakamila1717@gmail.com>
2024-09-09 21:48:13 +0700

    selesai

--- script.js
+++ script.js
@@ -1,28 +1,45 @@
-let number1 = [1,4,6,7]
+let number1 = [1,4,6,7];
+let number2 = [7,4,2,1];
+let number3 = [5];
+let number4 = [1,3];
+
+
+
 
 function numberKaliDua (arr) {
     return arr.map(num => num *2);
 }
 
 let hasil1 = numberKaliDua(number1);
+let hasil2 = numberKaliDua(number2);
+let hasil3 = numberKaliDua(number3);
+let hasil4 = numberKaliDua(number3);
 
 
-console.log(hasil1);
-console.log(hasil2);
-console.log(hasil3);
-console.log(hasil4);
+
+console.log(hasil1.reverse());
+console.log(hasil2.reverse());
+console.log(hasil3.reverse());
+console.log(hasil4.reverse());
 
 
-let arrSalinan = [...numberHasil]
 
-function numberUrut (arr) {
-    let arrSalinan = [arr];
-    return arrSalinan.reverse ();  
-}
+// console.log(hasil1);
+// console.log(hasil2);
+// console.log(hasil3);
+// console.log(hasil4);
 
 
-let hasil1 = numberUrut(number1);
-let hasil2 = numberUrut(number2);
-let hasil3 = numberUrut(number3);
-let hasil4 = numberUrut(number4);
+// let arrSalinan = [...numberHasil]
+
+// function numberUrut (arr) {
+//     let arrSalinan = [arr];
+//     return arrSalinan.reverse ();  
+// }
+
+
+// let hasil1 = numberUrut(number1);
+// let hasil2 = numberUrut(number2);
+// let hasil3 = numberUrut(number3);
+// let hasil4 = numberUrut(number4);
 
