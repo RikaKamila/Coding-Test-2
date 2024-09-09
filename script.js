8a92c5905d89cf347640900750a873cbd2def33c
Rika Kamila <rikakamila1717@gmail.com>
2024-09-09 21:49:54 +0700

    done

--- script.js
+++ script.js
@@ -24,22 +24,3 @@ console.log(hasil4.reverse());
 
 
 
-// console.log(hasil1);
-// console.log(hasil2);
-// console.log(hasil3);
-// console.log(hasil4);
-
-
-// let arrSalinan = [...numberHasil]
-
-// function numberUrut (arr) {
-//     let arrSalinan = [arr];
-//     return arrSalinan.reverse ();  
-// }
-
-
-// let hasil1 = numberUrut(number1);
-// let hasil2 = numberUrut(number2);
-// let hasil3 = numberUrut(number3);
-// let hasil4 = numberUrut(number4);
-
